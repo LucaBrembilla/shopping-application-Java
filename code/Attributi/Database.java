9149bdf8dde907456ab18edf0d45d17f6cff7022
Alessandro Colombo <93034575+AleColo01@users.noreply.github.com>
2022-01-10 13:02:12 +0100

    Update Database.java

--- code/Attributi/Database.java
+++ code/Attributi/Database.java
@@ -1,1 +1,7 @@
+package Attributi;
 
+public class Database {
+	public static final String HOST_URI = "jdbc:mysql://localhost:3306/pesos";
+	public static final String USER = "root";
+	public static final String PASSWORD = "";
+}
