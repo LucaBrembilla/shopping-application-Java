8fadb30bc11f72df6e248cb9f835da834e4ad116
Alessandro Colombo <93034575+AleColo01@users.noreply.github.com>
2022-01-20 17:06:35 +0100

    Update Theme.java

--- code/Attributi/Theme.java
+++ code/Attributi/Theme.java
@@ -1,20 +1,27 @@
 package Attributi;
 
+import javax.swing.*;
 import java.awt.*;
+
 
 public class Theme {
 	public static final int GUI_WIDTH = 800;
-	public static final int GUI_HEIGHT = 600;
+	public static final int GUI_HEIGHT = 675;
 	public static final int BUTTON_PRIMARY_WIDTH = 100;
+	
+	public static Image icon = new ImageIcon("Res/logo_large.png").getImage();
+	static Image newimg = icon.getScaledInstance(782, 150,  java.awt.Image.SCALE_SMOOTH);
+	public static final ImageIcon IMAGE_HEADER = new ImageIcon(newimg); 
 	
 	public static final Color BACKGROUND_PANEL = new Color(250, 250, 250);
 	public static final Color BACKGROUND_HEADER = new Color(0, 153, 0);
-	public static final Color COLOR_TITLE = new Color(0, 0, 0);
+	public static final Color COLOR_TITLE = new Color(136, 4, 4);
 	public static final Color COLOR_BUTTON_PRIMARY = new Color(255, 255, 255);
-	public static final Color BACKGROUND_BUTTON_PRIMARY = new Color(0, 153, 0);
+	public static final Color BACKGROUND_BUTTON_PRIMARY = new Color(136, 4, 4);
 	
 	public static final Font FONT_TITLE = new Font(Font.SANS_SERIF, Font.BOLD, 40);
 	public static final Font FONT_BUTTON = new Font(Font.SANS_SERIF, Font.BOLD, 14);
 	public static final Font FONT_REGULAR = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
 	public static final Font FONT_INPUT = new Font(Font.SANS_SERIF, Font.PLAIN, 16);
+    
 }
